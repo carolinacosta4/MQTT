f2d6454ecb80658b491e4594262ee0979ff4d35e
Beatriz Lisboa <btrzlisboa037@gmail.com>
2024-10-30 16:06:07 +0000

    fe done

--- src/views/Atendent.vue
+++ src/views/Atendent.vue
@@ -2,9 +2,9 @@
   <div id="general">
     <div id="left">
       <img class="logo" src="../assets/logo.svg" alt="">
-      <h2 v-if="queueStore.selectedRoute">Atendente - Gerenciamento de {{ queueStore.selectedRoute }}</h2>
+      <h2 class="green" v-if="queueStore.selectedRoute">Atendente - Gerenciamento de {{ queueStore.selectedRoute }}</h2>
       <div class="row space-between">
-        <h2>Services</h2>
+        <h2 class="green">Services</h2>
         <div class="row">
           <img class="icon" src="../assets/user.svg" alt="">
           <p class="text"> {{ total }}</p>
@@ -18,43 +18,100 @@
       </ul>
       <!-- tirar isso daqui -->
 
+      <div id="servicesList">
+        <div class="row space-between paddingLR"
+          v-if="queueStore.selectedRoute && queueStore.sectors[queueStore.selectedRoute]">
+          <p class="paddingLeft green">
+            Internacional
+          </p>
+          <div class="row">
+            <img class="icon" src="../assets/user.svg" alt="">
+            <p> {{ queueStore.sectors.internacional.clients.length }}</p>
+          </div>
+        </div>
 
-      <p v-if="queueStore.selectedRoute && queueStore.sectors[queueStore.selectedRoute]">
-        Total Clients in {{ queueStore.selectedRoute }}: {{ queueStore.sectors[queueStore.selectedRoute].clients.length
-        }} <!-- esse -->
-      </p>
+        <hr class="servicesList">
 
 
+        <div class="row space-between paddingLR"
+          v-if="queueStore.selectedRoute && queueStore.sectors[queueStore.selectedRoute]">
+          <p class="paddingLeft green">
+            Secretaria
+          </p>
+          <div class="row">
+            <img class="icon" src="../assets/user.svg" alt="">
+            <p> {{ queueStore.sectors.secretaria.clients.length }}</p>
+          </div>
+        </div>
 
-      <p v-if="queueStore.selectedRoute && queueStore.sectors[queueStore.selectedRoute]">
-        Total Clients in internacional: {{ queueStore.sectors.internacional.clients.length }}
-      </p>
-      <p v-if="queueStore.selectedRoute && queueStore.sectors[queueStore.selectedRoute]">
-        Total Clients in secretaria: {{ queueStore.sectors.secretaria.clients.length }}
-      </p>
-      <p v-if="queueStore.selectedRoute && queueStore.sectors[queueStore.selectedRoute]">
-        Total Clients in direcao: {{ queueStore.sectors.direcao.clients.length }}
-      </p>
-      <!-- <p v-if="queueStore.selectedRoute && queueStore.sectors[queueStore.selectedRoute]">
-        Total Clients in CPR: {{ queueStore.sectors.centro-producao-recursos.clients.length }}
-      </p> -->
-      <p v-if="queueStore.selectedRoute && queueStore.sectors[queueStore.selectedRoute]">
-        Total Clients in biblioteca: {{ queueStore.sectors.biblioteca.clients.length }}
-      </p>
-      <!-- <p v-if="queueStore.selectedRoute && queueStore.sectors[queueStore.selectedRoute]">
-        Total Clients in servicos-acao-social: {{ queueStore.sectors.servicos-acao-social.clients.length }}
-      </p> -->
+        <hr class="servicesList">
+
+
+        <div class="row space-between paddingLR"
+          v-if="queueStore.selectedRoute && queueStore.sectors[queueStore.selectedRoute]">
+          <p class="paddingLeft green">
+            Direção
+          </p>
+          <div class="row">
+            <img class="icon" src="../assets/user.svg" alt="">
+            <p> {{ queueStore.sectors.direcao.clients.length }}</p>
+          </div>
+        </div>
+
+        <hr class="servicesList">
+
+        <div class="row space-between paddingLR"
+          v-if="queueStore.selectedRoute && queueStore.sectors[queueStore.selectedRoute]">
+          <p class="paddingLeft green">
+            CPR
+          </p>
+          <div class="row">
+            <img class="icon" src="../assets/user.svg" alt="">
+            <!-- <p> {{ queueStore.sectors.centro-producao-recursos.clients.length }}</p> -->
+            <p>0</p>
+          </div>
+        </div>
+
+        <hr class="servicesList">
+
+        <div class="row space-between paddingLR"
+          v-if="queueStore.selectedRoute && queueStore.sectors[queueStore.selectedRoute]">
+          <p class="paddingLeft green">
+            Biblioteca
+          </p>
+          <div class="row">
+            <img class="icon" src="../assets/user.svg" alt="">
+            <p> {{ queueStore.sectors.biblioteca.clients.length }}</p>
+          </div>
+        </div>
+
+        <hr class="servicesList">
+
+        <div class="row space-between paddingLR"
+          v-if="queueStore.selectedRoute && queueStore.sectors[queueStore.selectedRoute]">
+          <p class="paddingLeft green">
+            Serviços ação social
+          </p>
+          <div class="row">
+            <img class="icon" src="../assets/user.svg" alt="">
+            <!-- <p> {{ queueStore.sectors.servicos-acao-social.clients.length }}</p> -->
+            <p>0</p>
+          </div>
+        </div>
+      </div>
+
+      <button id="finishServiceButton">Finish service</button>
     </div>
 
     <div id="right">
       <div id="ticketForm">
         <div id="upperPartTicket" class="centerPadding3">
-          <p class="grey text">Current ticket</p>
+          <p class="grey font-wheight-600 text">Current ticket</p>
           <p class="currentTicketNumber"
             v-if="queueStore.selectedRoute && queueStore.sectors[queueStore.selectedRoute]">
             {{ queueStore.sectors[queueStore.selectedRoute].currentTicket }}
           </p>
-          <h3>{{ queueStore.selectedRoute }}</h3>
+          <h2 class="green">{{ queueStore.selectedRoute }}</h2>
         </div>
 
         <div id="divider">
@@ -63,11 +120,15 @@
           <span class="dot"></span>
         </div>
 
-        <div id="bottomPartTicket" class="centerPadding3">
-          <button v-if="queueStore.selectedRoute && queueStore.sectors[queueStore.selectedRoute]"
+        <div id="bottomPartTicket" class="centerPadding4">
+          <div id="minorButtons">
+            <button id="cancelButton" class="minorActionButton font-wheight-700">Cancel</button>
+            <button class="minorActionButton font-wheight-700">Recall</button>
+          </div>
+          <button id="nextTicketButton" class="font-wheight-700" v-if="queueStore.selectedRoute && queueStore.sectors[queueStore.selectedRoute]"
             @click="queueStore.callNextTicket(queueStore.selectedRoute)"
             :disabled="!queueStore.sectors[queueStore.selectedRoute].clients.length">
-            Chamar Próxima Senha
+            Next ticket
           </button>
         </div>
       </div>
@@ -110,6 +171,17 @@ export default {
 </script>
 
 <style scoped>
+@font-face {
+  font-family: "Karla";
+  /* font-style: normal; */
+  /* font-weight: 400; */
+  src: url(https://fonts.gstatic.com/s/karla/v31/qkB9XvYC6trAT55ZBi1ueQVIjQTD-JrIH2G7nytkHRyQ8p4wUjm6bnEr.woff2) format('woff2');
+}
+
+* {
+	font-family: "Karla", sans-serif;
+}
+
 :root {
   background-color: #307e69 !important;
   width: 100vw !important;
@@ -128,6 +200,9 @@ html {
   align-items: center;
   justify-content: center;
   overflow: hidden;
+}
+p{
+  font-size: 18px;
 }
 
 #general {
@@ -154,13 +229,13 @@ html {
 img.logo {
   max-width: 50px;
   max-height: 50px;
-  justify-self: center;
+  margin-left: 40%;
 }
 
 .row {
   display: flex;
   flex-direction: row;
-  align-items: baseline;
+  align-items: center;
 }
 
 .space-between {
@@ -180,7 +255,18 @@ img.logo {
 
 .grey {
   color: #949983;
+}
+
+.green {
+  color: #307E69;
+}
+
+.font-wheight-600{
   font-weight: 600;
+}
+
+.font-wheight-700{
+  font-weight: 700;
 }
 
 #ticketForm {
@@ -231,6 +317,84 @@ hr {
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: space-around;
   padding: 8%;
+  width: 85%;
+}
+
+.centerPadding4 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  padding-left: 8%;
+  padding-right: 8%;
+  padding-bottom: 8%;
+  width: 85%;
+}
+
+#minorButtons {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+
+#nextTicketButton {
+  width: 100%;
+  padding: 4%;
+  border: none;
+  border-radius: 10px;
+  background-color: #307E69;
+  color: white;
+  margin-top: 5%;
+  font-size: 18px;
+}
+
+#cancelButton {
+  color: red;
+}
+
+#servicesList {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  background-color: white;
+  border-radius: 10px;
+  padding-top: 1%;
+  padding-bottom: 1%;
+}
+
+
+hr.servicesList {
+  border: none;
+  border-top: 1px solid #E7E8E3;
+  width: 100%;
+}
+
+.paddingLR {
+  padding-left: 2em;
+  padding-right: 2em;
+}
+
+#finishServiceButton {
+  padding: 4%;
+  width: 100%;
+  border: none;
+  border-radius: 100px;
+  background-color: red;
+  color: white;
+  margin-top: 8%;
+  font-weight: 700;
+  font-size: 18px;
+}
+
+.minorActionButton {
+  background-color: #E7E8E3;
+  border-radius: 10px;
+  border: none;
+  color: #307E69;
+  width: 48%;
+  padding: 4%;
+  font-size: 18px;
 }
 </style>
